2e3f578323cb36f6283456e2d8408a8f068bff5f
TheWako <jean-philippe.isungu@supinternet.fr>
2017-01-08 13:22:35 +0100

    fos user installed

---
+++ src/IntranetBundle/Entity/Report.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace IntranetBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Report
+ *
+ * @ORM\Table(name="report")
+ * @ORM\Entity(repositoryClass="IntranetBundle\Repository\ReportRepository")
+ */
+class Report
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="Grade", type="float")
+     */
+    private $grade;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set grade
+     *
+     * @param float $grade
+     *
+     * @return Report
+     */
+    public function setGrade($grade)
+    {
+        $this->grade = $grade;
+
+        return $this;
+    }
+
+    /**
+     * Get grade
+     *
+     * @return float
+     */
+    public function getGrade()
+    {
+        return $this->grade;
+    }
+}
+
